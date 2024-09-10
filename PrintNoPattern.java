11a4a31627172b1cf833eb0a23cf73bf39d8c50c
AishwaryaRGaikwad <121550532+AishwaryaRGaikwad@users.noreply.github.com>
2024-09-10 17:56:48 +0530

    Add files via upload

---
+++ PrintNoPattern.java
@@ -0,0 +1,20 @@
+
+public class PrintNoPattern {
+
+	public static void main(String[] args) {
+
+        int i,j,num=1;
+        for(i=0;i<=3;i++)
+        {
+        	for(j=0;j<=3;j++)
+        	{
+        		System.out.print(num);
+        	}
+        	System.out.println(" ");
+        	num++;
+        }
+		}
+	
+	}
+
+
